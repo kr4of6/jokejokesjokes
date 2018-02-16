843c79a49d936d118ec83007dd64911a580793a0
Kathryn Rasmussen <kathryn.rasmussen12@gmail.com>
2018-02-15 21:01:30 -0700

    epic

--- script.js
+++ script.js
@@ -1,72 +1,86 @@
 $(document).ready(function () {
 
-    var getRandJoke = function(e)
-    {
+    var getRandJoke = function (e) {
         e.preventDefault();
         var result = ""
         var myurl = "https://icanhazdadjoke.com/"
         $.ajax({
-            url : myurl,
-            dataType : "json",
-            success : function(json)
-            {
+            url: myurl,
+            dataType: "json",
+            success: function (json) {
                 console.log(json.joke);
-                result +=`<div class="joke">` + json.joke + `</div>`;
+                result += `<div class="joke">` + json.joke + `</div>`;
                 $('#rResults').html(result);
             }
         });
-        
-        
+
+
     };
-    var getSearchResults = function(e)
-    {
+    var getSearchResults = function (e) {
         e.preventDefault();
+
+
+
         var result = '';
         var value = $('#searchText').val();
-        console.log(value);
+        // console.log(value);
         var myurl = "https://icanhazdadjoke.com/search?term=" + value;
         $.ajax({
-            url : myurl,
-            dataType : "json",
-            success : function(json)
-            {
+            url: myurl,
+            dataType: "json",
+            success: function (json) {
 
-                if (json.results.length === 0){
-                    result +=`<div class="joke">Sorry, no jokes found</div>`;
-                }else {
+                var mygifurl = "http://api.giphy.com/v1/gifs/search?q=" + value + "&api_key=G6b28WhJndDhJ5p2Ey4LNR0eGGRkQFHg&limit=5";
+                $.ajax({
+                    url: mygifurl,
+                    dataType: "json",
+                    success: function (jsonGif) {
+                        // for(var i = 0 ; i < json.data.length ; i++)
+                        // {
+                        // }
+                        // $('#sResults').html(result);
 
-                    for(var i = 0 ; i < json.results.length ; i++)
-                    {
-                        console.log(json.results[i].joke);
-                        result +=`<div class="joke">` + json.results[i].joke + `</div>`;
-                        result += "<hr>"
+
+                        if (json.results.length === 0) {
+                            result += `<div class="joke">Sorry, no jokes found</div>`;
+                        } else {
+
+                            for (var i = 0; i < json.results.length && i < jsonGif.data.length; i++) {
+                                // console.log(json.results[i].joke);
+                                result += `<div class="joke">`
+                                result += '<img src="' + jsonGif.data[i].images.fixed_height.url + '">'
+                                result += `<p>` + json.results[i].joke + `<p>`;
+                                result += `</div>`;
+                                result += "<hr>"
+                            }
+                        }
+                        $('#sResults').html(result);
                     }
-                }
-                $('#sResults').html(result);
+                });
+
             }
         });
     }
-    var getGif = function(e)
-    {
+    var getGif = function (e) {
         var result = "";
         e.preventDefault();
         var value = $('#searchText').val();
-        var myurl = "http://api.giphy.com/v1/gifs/search?q="+value+ "&api_key=G6b28WhJndDhJ5p2Ey4LNR0eGGRkQFHg&limit=5";  
+        var myurl = "http://api.giphy.com/v1/gifs/search?q=" + value + "&api_key=G6b28WhJndDhJ5p2Ey4LNR0eGGRkQFHg&limit=5";
         $.ajax({
-            url : myurl,
-            dataType : "json",
-            success : function(json)
-            {
-                // console.log(json);
-                for(var i = 0 ; i < json.data.length ; i++)
-                {
-                result += '<img src="' + json.data[i].images.fixed_height.url + '">'
-                }
-                $('#sResults').html(result);
+            url: myurl,
+            dataType: "json",
+            success: function (json) {
+                console.log(json);
+                // for(var i = 0 ; i < json.data.length ; i++)
+                // {
+                // result += '<img src="' + json.data[i].images.fixed_height.url + '">'
+                // }
+                // $('#sResults').html(result);
+                return json;
             }
         });
     }
     $('#searchSubmit').click(getSearchResults);
     $('#rand').click(getRandJoke);
     $('#try').click(getGif);
- });
+});
